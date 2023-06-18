63679971f13a47a6ed4b4bf8ece26e7736ed0aa4
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-06-18 11:46:15 +1000

    Feast : API error message

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -2,10 +2,31 @@ import { useState, useEffect } from "react";
 import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
+import { projectAPI} from "./projectAPI";
 
 const ProjectsPage = () => {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
-    
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string|undefined>(undefined);
+
+    useEffect(() => {
+        setLoading(true);
+        projectAPI
+            .get(1)
+            .then((data)=>{
+                setError('');
+                setLoading(false);
+                setProjects(data);
+            })
+            .catch((e)=>{
+                setLoading(false);
+                setError(e.message);
+                if(e instanceof Error){
+                    setError(e.message)
+                }
+            });
+    }, []);
+
 
     const saveProject = (project:Project)=>{
         console.log("Saving Project", project);
@@ -18,10 +39,25 @@ const ProjectsPage = () => {
     return(
         <>
             <h1>Projects</h1>
+            {error &&(
+                <div className="card large error">
+                    <section>
+                        <p>
+                            <span className="icon-altert inverse"></span>
+                            {error}
+                        </p>
+                    </section>
+                </div>
+            )}
             <ProjectList 
             onSave={ saveProject }
             projects={projects}/>
-            
+            {loading &&(
+                <div className="center-page">
+                    <span className="spinner primary"></span>
+                    <p>Loading ... </p>
+                </div>
+            )}
         </>
     )
 };
